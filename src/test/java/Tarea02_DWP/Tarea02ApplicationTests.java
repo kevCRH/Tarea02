d3c1121caf57eaa9617f004155ddc25e13f7c235
kevCRH <101358922+kevCRH@users.noreply.github.com>
2022-06-06 18:15:33 -0600

    Commit Tarea02. Completa

---
+++ src/test/java/Tarea02_DWP/Tarea02ApplicationTests.java
@@ -0,0 +1,13 @@
+package Tarea02_DWP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
